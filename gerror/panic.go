66bf473a3475c17c7ceec4f609d06358d75570ee
bankonly <for21bn01@gmail.com>
2022-11-21 19:15:02 +0700

    1.0.17

--- gerror/panic.go
+++ gerror/panic.go
@@ -16,3 +16,42 @@ func Panic(statusCode int, p E) {
 	}
 	panic(fmt.Sprintf("%s-%s::%s", strconv.Itoa(statusCode), p.ErrorCode, p.Message))
 }
+
+type Throw struct {
+	Message   string
+	ErrorCode string
+}
+
+func (e E) BadRequest() {
+	if e.ErrorCode == "" {
+		e.ErrorCode = "4000"
+	}
+	if e.Message == "" {
+		e.Message = "Bad request"
+	}
+	panic(fmt.Sprintf("%s-%s::%s", strconv.Itoa(400), e.ErrorCode, e.Message))
+}
+
+func (e E) Unauthorized() {
+	if e.ErrorCode == "" {
+		e.ErrorCode = "4001"
+	}
+	if e.Message == "" {
+		e.Message = "Unauthorized"
+	}
+	panic(fmt.Sprintf("%s-%s::%s", strconv.Itoa(400), e.ErrorCode, e.Message))
+}
+
+func (e E) Custom() {
+	panic(fmt.Sprintf("%s-%s::%s", strconv.Itoa(400), e.ErrorCode, e.Message))
+}
+
+func (e E) InteralServerError() {
+	if e.ErrorCode == "" {
+		e.ErrorCode = "5000"
+	}
+	if e.Message == "" {
+		e.Message = "Interal server error"
+	}
+	panic(fmt.Sprintf("%s-%s::%s", strconv.Itoa(400), e.ErrorCode, e.Message))
+}
